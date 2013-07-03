0cef4967d82c3e19edea5c5ef340e45385c83a0e
Niklas Bergius <niklas@bergius.org>
2013-07-03 06:41:18 +0200

    go fmt

--- main.go
+++ main.go
@@ -1,35 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/garyburd/redigo/redis"
+	"io/ioutil"
 	"log"
+	"os/exec"
+	"path"
 	"regexp"
 	"strconv"
-	"path"
-	"os/exec"
-	"encoding/json"
-	"io/ioutil"
-	"github.com/garyburd/redigo/redis"
-  "sync"
+	"sync"
 )
 
 type Cfg struct {
-	ManagerPath string
-	ListenPort int
-  LandlordPort int
+	ManagerPath  string
+	ListenPort   int
+	LandlordPort int
 }
 
 type Instruction struct {
-  ReplyTo string
-  Op string
-  Id string
+	ReplyTo string
+	Op      string
+	Id      string
 }
 
 type SetupResponse struct {
-  Id string
-  Status string
-  Error string
-  Port int
+	Id     string
+	Status string
+	Error  string
+	Port   int
 }
 
 var cfg Cfg
@@ -43,50 +43,50 @@ func readConfig() *Cfg {
 	var cfg Cfg
 	json.Unmarshal(file, &cfg)
 
-  if cfg.ManagerPath == "" {
-    log.Fatalf("Strange config: %v", cfg)
-  }
+	if cfg.ManagerPath == "" {
+		log.Fatalf("Strange config: %v", cfg)
+	}
 
-  if cfg.ListenPort == 0 {
-    cfg.ListenPort = 8080
-  }
+	if cfg.ListenPort == 0 {
+		cfg.ListenPort = 8080
+	}
 
-  if cfg.LandlordPort == 0 {
-    cfg.LandlordPort = 6380
-  }
+	if cfg.LandlordPort == 0 {
+		cfg.LandlordPort = 6380
+	}
 
-  log.Printf("Config: %v", cfg)
+	log.Printf("Config: %v", cfg)
 
 	return &cfg
 }
 
 func toJson(obj interface{}) *[]byte {
-  r, e := json.Marshal(obj)
-  if e != nil {
-    log.Panicf("Unable to marshal %v: %v", obj, e)
-  }
+	r, e := json.Marshal(obj)
+	if e != nil {
+		log.Panicf("Unable to marshal %v: %v", obj, e)
+	}
 
-  return &r
+	return &r
 }
 
 func dial() *redis.Conn {
-	c, e := redis.Dial("tcp", ":" + strconv.Itoa(cfg.LandlordPort))
+	c, e := redis.Dial("tcp", ":"+strconv.Itoa(cfg.LandlordPort))
 	if e != nil {
-    log.Panicf("Dialling error: %v", e)
+		log.Panicf("Dialling error: %v", e)
 	}
 
-  if _, e := c.Do("AUTH", "landlord"); e != nil {
-    log.Panicf("AUTH error: %v", e)
-  }
+	if _, e := c.Do("AUTH", "landlord"); e != nil {
+		log.Panicf("AUTH error: %v", e)
+	}
 
-  return &c
+	return &c
 }
 
 func setup(id string) (int, error) {
-  c := *dial()
-  defer c.Close()
+	c := *dial()
+	defer c.Close()
 
-  log.Printf("Setting up \"%s\"", id)
+	log.Printf("Setting up \"%s\"", id)
 
 	port, _ := redis.Int(c.Do("INCR", "port"))
 	port = 6380 + port
@@ -117,79 +117,79 @@ func setup(id string) (int, error) {
 }
 
 func dispatchResponse(recipient string, rsp interface{}) {
-  c := *dial()
-  defer c.Close()
+	c := *dial()
+	defer c.Close()
 
-  log.Printf("Responding to %s: %v", recipient, rsp)
-  payload := toJson(rsp)
-  c.Do("PUBLISH", "landlord.response." + recipient, string(*payload))
+	log.Printf("Responding to %s: %v", recipient, rsp)
+	payload := toJson(rsp)
+	c.Do("PUBLISH", "landlord.response."+recipient, string(*payload))
 }
 
 func readInstruction(msg *redis.Message) *Instruction {
-  var instr Instruction
-  if e := json.Unmarshal(msg.Data, &instr); e != nil {
-    log.Printf("Unable to read instruction \"%s\"; %v", msg.Data, e)
-    return nil
-  }
+	var instr Instruction
+	if e := json.Unmarshal(msg.Data, &instr); e != nil {
+		log.Printf("Unable to read instruction \"%s\"; %v", msg.Data, e)
+		return nil
+	}
 
-  return &instr
+	return &instr
 }
 
 func handleInstruction(instr *Instruction) {
-  switch instr.Op {
-  case "Setup":
-    rsp := new(SetupResponse)
-    rsp.Id = instr.Id
-    port, e := setup(instr.Id)
-    if e != nil {
-      rsp.Status = "ERROR"
-      rsp.Error = e.Error()
-    } else {
-      rsp.Status = "OK"
-      rsp.Port = port
-    }
+	switch instr.Op {
+	case "Setup":
+		rsp := new(SetupResponse)
+		rsp.Id = instr.Id
+		port, e := setup(instr.Id)
+		if e != nil {
+			rsp.Status = "ERROR"
+			rsp.Error = e.Error()
+		} else {
+			rsp.Status = "OK"
+			rsp.Port = port
+		}
 
-    dispatchResponse(instr.ReplyTo, rsp)
-  default:
-    log.Printf("Unknown op: %s", instr.Op)
-  }
+		dispatchResponse(instr.ReplyTo, rsp)
+	default:
+		log.Printf("Unknown op: %s", instr.Op)
+	}
 }
 
 func listen() {
-  c := *dial()
-  defer c.Close()
+	c := *dial()
+	defer c.Close()
 
-  var wg sync.WaitGroup
-  wg.Add(1)
+	var wg sync.WaitGroup
+	wg.Add(1)
 
-  psc := redis.PubSubConn{c}
+	psc := redis.PubSubConn{c}
 
-  go func() {
-    psc.Subscribe("landlord.request")
-    defer wg.Done()
-    for {
-      switch v := psc.Receive().(type) {
-      case redis.Message:
-        log.Printf("%s: message %s", v.Channel, v.Data)
-        if instr := readInstruction(&v); instr != nil {
-          handleInstruction(instr)
-        }
-      case error:
-        log.Printf("Receive fail; %v", v)
-        return
-      }
-    }
-  }()
+	go func() {
+		psc.Subscribe("landlord.request")
+		defer wg.Done()
+		for {
+			switch v := psc.Receive().(type) {
+			case redis.Message:
+				log.Printf("%s: message %s", v.Channel, v.Data)
+				if instr := readInstruction(&v); instr != nil {
+					handleInstruction(instr)
+				}
+			case error:
+				log.Printf("Receive fail; %v", v)
+				return
+			}
+		}
+	}()
 
-  wg.Wait()
-  return
+	wg.Wait()
+	return
 }
 
 var idValidator = regexp.MustCompile("^[_\\-a-zA-Z0-9]+$")
 
 func main() {
-  cfg = *readConfig()
-  listen()
+	cfg = *readConfig()
+	listen()
 
 	log.Println("Exited")
 	fmt.Println("Good bye.")
